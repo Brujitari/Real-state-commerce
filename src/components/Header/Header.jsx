c1f732a0123e08dd1442446e993a18553094dc39
Nati Montes <natimontesg2@gmail.com>
2023-12-20 02:15:20 +0100

    feat: Responsive menu, logic error

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,34 @@
-import React from "react";
-import './Header.css'
+import React, { useState } from "react";
+import "./Header.css";
+import { BiMenuAltRight } from "react-icons/bi";
+import OutsideClickHandler from "react-outside-click-handler";
 
 const Header = () => {
+  const [openMenu, setOpenMenu] = useState(false)
+  console.log(openMenu)
   return (
     <section className="h-wrapper">
       <div className="flexCenter paddings innerWidth h-container">
-        <img src="./logo.png" alt="word Homyz in front of black diagonal lines with a blue accent" width= {100} />
-        <div className="flexCenter h-menu">
-            <a href="">Residencies</a>
-            <a href="">Our Value</a>
-            <a href="">Contact Us</a>
-            <a href="">Get Started</a>
-            <button className="button">
+        <img
+          src="./logo.png"
+          alt="word Homyz in front of black diagonal lines with a blue accent"
+          width={100}
+        />
+        <OutsideClickHandler onOutsideClick={() => {
+          setOpenMenu(false)
+        }}>
+        <div className={`flexCenter h-menu ${openMenu ? '' : 'hidden'}`}>
+          <a href="">Residencies</a>
+          <a href="">Our Value</a>
+          <a href="">Contact Us</a>
+          <a href="">Get Started</a>
+          <button className="button">
             <a href="">Contact</a>
-            </button>
+          </button>
+        </div>
+        </OutsideClickHandler>
+        <div className="menu-icon" onClick={() => setOpenMenu (prev => !prev)}>
+          <BiMenuAltRight size={30} />
         </div>
       </div>
     </section>
